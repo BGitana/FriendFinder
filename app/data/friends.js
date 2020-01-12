c97b4b360d151b13f5357e1a630a5fce633b2ec8
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-01-12 00:33:46 -0500

    Reaformated .js files

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,167 +5,57 @@
 // But it could have been an empty array as well.
 // ===============================================================================
 var friends = [
-    {
-        "name":"Owen",
-        "photo":"https://media.giphy.com/media/1skTYzBWynK1y/giphy.gif",
-        "scores":[
-           5,
-           1,
-           4,
-           4,
-           5,
-           1,
-           2,
-           5,
-           4,
-           1
-            ]
-        },
-    {
-    "name":"Pavan",
-    "photo":"https://media.giphy.com/media/7q9LxggEzRiOk/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       4,
-       5,
-       1,
-       2,
-       5,
-       4,
-       1
-        ]
+  {
+    name: "Owen",
+    photo: "https://media.giphy.com/media/1skTYzBWynK1y/giphy.gif",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    "name":"Angel",
-    "photo":"https://media.giphy.com/media/AfpEltQudBg5y/giphy.gif",
-    "scores":[
-       9,
-       1,
-       4,
-       4,
-       5,
-       1,
-       2,
-       5,
-       4,
-       1
-        ]
+    name: "Pavan",
+    photo: "https://media.giphy.com/media/7q9LxggEzRiOk/giphy.gif",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    "name":"Karina",
-    "photo":"https://media.giphy.com/media/8SCntm1JiCS2s/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Angel",
+    photo: "https://media.giphy.com/media/AfpEltQudBg5y/giphy.gif",
+    scores: [9, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    "name":"Tommy",
-    "photo":"https://media.giphy.com/media/xOBhCxh7Dk6SA/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Karina",
+    photo: "https://media.giphy.com/media/8SCntm1JiCS2s/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Lisa",
-    "photo":"https://media.giphy.com/media/2ca9aemQxIQXS/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Tommy",
+    photo: "https://media.giphy.com/media/xOBhCxh7Dk6SA/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Nick",
-    "photo":"https://media.giphy.com/media/9OnUuWyjZ4VSU/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Lisa",
+    photo: "https://media.giphy.com/media/2ca9aemQxIQXS/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Jason",
-    "photo":"https://media.giphy.com/media/9M36yTC8BlRPG/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Nick",
+    photo: "https://media.giphy.com/media/9OnUuWyjZ4VSU/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Nate",
-    "photo":"https://media.giphy.com/media/6mwSgAGomAo7K/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Jason",
+    photo: "https://media.giphy.com/media/9M36yTC8BlRPG/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Ken",
-    "photo":"https://media.giphy.com/media/tTLvTaZruCBoc/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Nate",
+    photo: "https://media.giphy.com/media/6mwSgAGomAo7K/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
+  },
+  {
+    name: "Ken",
+    photo: "https://media.giphy.com/media/tTLvTaZruCBoc/giphy.gif",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   }
-  ]
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friends;
+];
+
+//Below to export the array. This makes it accessible to other files using require.
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give the server useful functionality
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 // ==============================================================================
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server 
+// This sets up the basic properties for our express server
 
 var app = express(); // Tells node that we are creating an "express" server
 var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
@@ -14,32 +14,28 @@ var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this lat
 // BodyParser makes it easy for our server to interpret data sent to it.
 // The code below is pretty standard.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or 
-// request data from various URLs. 
+// These routes give our server a "map" of how to respond when users visit or
+// request data from various URLs.
 // ================================================================================
 
-require('./app/routing/apiRoutes.js')(app); 
-require('./app/routing/htmlRoutes.js')(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 // var htmlRoutes = require('./app/routing/htmlRoutes')(app)
 // var apiRoutes = require('./app/routing/apiRoutes')(app)
 
 // ==============================================================================
 // LISTENER
-// The below code effectively "starts" our server 
+// The below code effectively "starts" our server
 // ==============================================================================
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
-
-
-
-
